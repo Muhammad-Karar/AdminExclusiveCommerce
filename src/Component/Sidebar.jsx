a4db8cbc49af8b63b67ac492b3dbf57216261d8d
Muhammad-Karar <102608977+Muhammad-Karar@users.noreply.github.com>
2024-04-02 22:57:52 +0500

    .

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   return (
-    <div className='py-7 px-11 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-center w-full bg-stone-800 lg:gap-x-4 lg:flex lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6'>
+    <div className='py-7 px-7 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-center w-full bg-stone-800 lg:gap-x-4 lg:flex lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6'>
 
   <Link to={'/addProduct'}>
     <button className='flexCenter gap-2 rounded-md bg-red-500 h-14 w-40 medium-16'>
