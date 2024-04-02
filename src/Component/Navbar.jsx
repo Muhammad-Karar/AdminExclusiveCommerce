228e054892d0aab47a38c8efe51a26fcc6169a37
Muhammad-Karar <102608977+Muhammad-Karar@users.noreply.github.com>
2024-04-02 22:36:26 +0500

    push

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,8 +4,8 @@ import p2 from '../assets/p2.jpg'
 
 export default function Navbar() {
     return (
-        <nav className='max_padd_container flex justify-between bg-red-500 px-11 py-2 ring-1 ring-slate-900/5 relative'>
-            <div className='uppercase font-bold text-red-500 bg-stone-800 rounded-md px-3 py-3 mt-1 h-12 text-sm font-medium'>Admin Exclusive Commerce</div>
+        <nav className='flex md:flex-row justify-between bg-red-500 md:px-11 py-2 ring-1 ring-slate-900/5 fix'>
+            <div className='uppercase font-bold text-white bg-stone-800 rounded-md px-3 py-3 mt-1 h-12 text-sm font-medium'>Admin Exclusive Commerce</div>
 
             <div className='flex items-center'>
                 <div><img src={p2} alt="" className='h-14 w-14 rounded-full' /></div>
--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   return (
-    <div className='py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-stone-800 sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen
+    <div className='py-7 flex flex-col sm:flex-row justify-center gap-x-1 gap-y-5 w-full bg-stone-800 sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen
     lg:justify-start lg:pl-6'>
 
       <Link to={'/addProduct'}>
