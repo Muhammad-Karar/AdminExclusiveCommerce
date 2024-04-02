7cd7e64726d59e5be520c9d7efd84b0c688ede4a
Muhammad-Karar <102608977+Muhammad-Karar@users.noreply.github.com>
2024-04-02 22:13:42 +0500

    push

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import p2 from '../assets/p2.jpg'
-import { ChevronDownIcon } from '@heroicons/react/24/outline';
+// import { ChevronDownIcon } from '@heroicons/react/24/outline'
 
 export default function Navbar() {
     return (
-        <nav className='max_padd_container flex justify-between bg-gray-900 px-11 py-2 ring-1 ring-slate-900/5 relative'>
-            <div className='uppercase bold-22 text-white bg-gray-800 rounded-md px-3 py-3 mt-1 h-12 text-sm font-medium'>Admin Exclusive Commerce</div>
+        <nav className='max_padd_container flex justify-between bg-red-500 px-11 py-2 ring-1 ring-slate-900/5 relative'>
+            <div className='uppercase font-bold text-red-500 bg-stone-800 rounded-md px-3 py-3 mt-1 h-12 text-sm font-medium'>Admin Exclusive Commerce</div>
 
             <div className='flex items-center'>
                 <div><img src={p2} alt="" className='h-14 w-14 rounded-full' /></div>
-                <ChevronDownIcon className="h-6 w-6 text-white mr-2" />
+                {/* <ChevronDownIcon class="h-6 w-6 text-gray-500" /> */}
             </div>
 
         </nav>
--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   return (
-    <div className='py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-gray-900 sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen
+    <div className='py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-stone-800 sm:gap-x-4 lg:flex-col lg:pt-20 lg:max-w-60 lg:h-screen
     lg:justify-start lg:pl-6'>
 
       <Link to={'/addProduct'}>
-        <button className='flexCenter gap-2 rounded-md bg-gray-800 h-14 w-44 medium-16'>
-            <span className='uppercase bold-22 text-white px-3 py-3 mt-1 text-sm font-medium'>Add Product</span>
+        <button className='flexCenter gap-2 rounded-md bg-red-500 h-14 w-44 medium-16'>
+            <span className='uppercase font-bold text-black px-3 py-3 mt-1 text-sm font-medium'>Add Product</span>
         </button>
       </Link>
       <Link to={'/addBestSelling'}>
-        <button className='flexCenter gap-2 rounded-md bg-gray-800 h-14 w-44 medium-16'>
-            <span className='uppercase bold-22 text-white py-3 mt-1 text-sm font-medium'>Add Best Selling Product</span>
+        <button className='flexCenter gap-2 rounded-md bg-red-500 h-14 w-44 medium-16'>
+            <span className='uppercase font-bold text-black py-3 mt-1 text-sm font-medium'>Add Best Selling Product</span>
         </button>
       </Link>
       <Link to={'/addFlashSale'}>
-        <button className='flexCenter gap-2 rounded-md bg-gray-800 h-14 w-44 medium-16'>
-            <span className='uppercase bold-22 text-white py-3 mt-1 text-sm font-medium'>Add Flash Sale Product</span>
+        <button className='flexCenter gap-2 rounded-md bg-red-500 h-14 w-44 medium-16'>
+            <span className='uppercase font-bold text-black py-3 mt-1 text-sm font-medium'>Add Flash Sale Product</span>
         </button>
       </Link>
       <Link to={'/addNewArrival'}>
-        <button className='flexCenter gap-2 rounded-md bg-gray-800 h-14 w-44 medium-16'>
-            <span className='uppercase bold-22 text-white py-3 mt-1 text-sm font-medium'>Add New Arrival Product</span>
+        <button className='flexCenter gap-2 rounded-md bg-red-500 h-14 w-44 medium-16'>
+            <span className='uppercase font-bold text-black py-3 mt-1 text-sm font-medium'>Add New Arrival Product</span>
         </button>
       </Link>
     </div>
